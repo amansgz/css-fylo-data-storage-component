8bcb768bf75e6d3e77b95dc95fd8bf839b4f1bd0
amansgz <amansgz.ar@gmail.com>
2023-09-08 20:45:36 -0300

    Update mobile styles

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-const innerBar = document.getElementById('storage-inner-bar');
+const innerBar = document.getElementById('storageInnerBar');
 
 innerBar.style.width = `${100/(1000 / 815)}%`;
 innerBar.style.backgroundImage = 'linear-gradient(hsl(6, 100%, 80%), hsl(335, 100%, 65%))';
